24a12c5eb129d476d6582499cf3d92fa40ab188c
HwangSlater <doorchan23@naver.com>
2026-06-10 22:48:42 +0900

    fix: hardcode UTF-8 file.encoding property in Main class for foolproof Windows execution

--- src/main/java/com/documentintelligenceapplication/DocumentIntelligenceApplication.java
+++ src/main/java/com/documentintelligenceapplication/DocumentIntelligenceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DocumentIntelligenceApplication {
 
     public static void main(String[] args) {
+        System.setProperty("file.encoding", "UTF-8");
         SpringApplication.run(DocumentIntelligenceApplication.class, args);
     }
 
